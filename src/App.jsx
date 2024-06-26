554c1f4cd02dca3006732d0a12ea4c47f09ab4fd
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-06-26 18:20:44 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <body>
+        <section className="info-form"></section>
+        <section className="cv"></section>
+      </body>
+    </>
+  );
+}
+
+export default App;
